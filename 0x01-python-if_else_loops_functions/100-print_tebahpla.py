5a79ef35e5acce488318e00e5e8e6277e7af1b82
FouedDadi <foued.dadii@gmail.com>
2020-04-28 21:19:31 +0100

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in reversed(range(65, 91)):
-    if  i % 2 == 0:
+    if i % 2 == 0:
         i = i + 32
-    print(chr(i), end = "")
+    print("{}".format(chr(i)), end="")
