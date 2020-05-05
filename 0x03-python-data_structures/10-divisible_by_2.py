c50388c73e1fb935e303937c107f73ea041cee2d
FouedDadi <foued.dadii@gmail.com>
2020-05-05 04:16:17 +0100

    all

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    mul2 = []
+    for x in my_list:
+        if my_list[x] % 2 == 0:
+            mul2.append(True)
+        elif my_list[x] % 2 != 0:
+            mul2.append(False)
+    return mul2
