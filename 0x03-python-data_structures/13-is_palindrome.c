7937f61314cbe4236f090581fcf16387ff992c77
FouedDadi <foued.dadii@gmail.com>
2020-05-06 14:48:12 +0100

    13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
+
+
+
+int is_palindrome(listint_t **head)
+{
+listint_t *t = *head;
+int x = 0, i = 0, y = 0, z[4000];
+if (!t || !t->next)
+    return (1);
+for (; t; x++, t = t->next)
+{
+    z[x] = t->n;
+}
+printf("%d\n",x);
+z[x + 1] = '\0';
+for (y = x - 1; y >= x / 2; y--, i++)
+{
+    /*printf("y = %d,i = %d\n",z[y], z[i]);*/
+    if (z[y] != z[i])
+        return (0);
+}
+return (1);
+}
