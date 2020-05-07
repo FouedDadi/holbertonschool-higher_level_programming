ede401547e742304f30d220d55f82b74ebe71fa8
FouedDadi <foued.dadii@gmail.com>
2020-05-07 18:48:12 +0100

    100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    S = 0
+    n = 0
+    for x in my_list:
+        S = (x[0] * x[1]) + S
+        n = n + x[1]
+    return S / n
