e054519036d6346d5384a2e359b838a6750abdc0
FouedDadi <foued.dadii@gmail.com>
2020-05-19 15:53:38 +0100

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+define a class Square
+"""
+
+
 class Square:
+    """
+    class Square with private instance attribute size
+    """
     def __init__(self, size):
+        """
+        size = size
+        """
         self.__size = size
