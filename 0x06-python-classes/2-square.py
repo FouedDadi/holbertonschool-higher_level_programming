0abbe2169866d297f7d9287fec5356c112d326c6
FouedDadi <foued.dadii@gmail.com>
2020-05-19 15:55:00 +0100

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+define a class Square
+"""
+
+
 class Square:
+    """
+    class Square with private instance attribute size
+    """
     def __init__(self, size=0):
+        """
+        size = size
+        """
         self.__size = size
         if size != int:
             raise TypeError('size must be an integer')
