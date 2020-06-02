2da30820c2a7061c9632a0bced4bc4c4ecf0a719
FouedDadi <foued.dadii@gmail.com>
2020-06-02 23:55:19 +0100

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+importing from 9-rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class square
+    """
+    def __init__(self, size):
+        """
+        initialisation
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        return area
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        return str
+        """
+        return "[{}] {}/{}"\
+            .format(self.__class__.__name__, self.__size, self.__size)
