4ef02d9bee0490e532aa6ee346b30e7721c4d7ae
FouedDadi <foued.dadii@gmail.com>
2020-06-04 09:48:57 +0100

    9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,9 +10,9 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    newlist = load_from_json_file('add_item.json')
+    newlist = load_from_json_file(filename)
 except:
-    newlist = list()
-for a in range(1, len(argv)):
-    newlist.append(argv[a])
-save_to_json_file(newlist, 'add_item.json')
+    newlist = []
+for x in range(1, len(argv)):
+    newlist.append(x)
+save_to_json_file(newlist, filename)
