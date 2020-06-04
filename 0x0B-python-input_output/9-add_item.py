af907d0d32e4ad9741f3412a04ce4fe4f0c10d27
FouedDadi <foued.dadii@gmail.com>
2020-06-04 09:36:43 +0100

    9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,9 +2,17 @@
 """
 script that adds all arguments to a Python list
 """
-import sys
+from sys import argv
+import json
 
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+try:
+    newlist = load_from_json_file('add_item.json')
+except:
+    newlist = list()
+for a in range(len(argv)):
+    newlist.append(argv[a])
+save_to_json_file(newlist, 'add_item.json')
