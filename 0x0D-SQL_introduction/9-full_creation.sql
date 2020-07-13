446dda0b11bb65e50e2e32ee665aec41a9d77ae0
FouedDadi <foued.dadii@gmail.com>
2020-07-13 18:51:29 +0100

    create base and add multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,6 @@
 -- script that creates a table second_table and adds multiple rows
-CREATE TABLE if not exists second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "Gorge", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, "John", 10),
+                                (2, "Alex", 3),
+                                (3, "Bob", 14),
+                                (4, "Gorge", 8);
