d4ea082669f2ae849433b4353122976e86dcd751
FouedDadi <foued.dadii@gmail.com>
2020-07-14 18:50:38 +0100

    create DB and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create a database hbtn_0d_usa
+-- create a table in hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, name VARCHAR(256));
