2f51b7db81d2b6b2f6c9baee4c061d889230e7c9
FouedDadi <foued.dadii@gmail.com>
2020-07-14 18:10:35 +0100

    show privileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script that show privileges of user 1 and user 2
--- privileges of mysql users
+-- privileges of user 1
+-- privileges of user 2
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
