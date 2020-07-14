929c25a64bb5b277976de2d245fb81250c45cfaf
FouedDadi <foued.dadii@gmail.com>
2020-07-14 18:56:34 +0100

    create DB and table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- create a database hbtn_0d_usa
 -- create a table in hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states NOT NULL, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id) NOT NULL, name VARCHAR(256) NOT NULL);
