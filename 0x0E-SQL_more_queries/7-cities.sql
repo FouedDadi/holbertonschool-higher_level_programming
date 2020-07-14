8c00e318b20e236feb23a2188227c6f06ef514ef
FouedDadi <foued.dadii@gmail.com>
2020-07-14 19:30:57 +0100

    create DB and table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create a database hbtn_0d_usa
--- create a table in hbtn_0d_usa
+-- create a table cities in hbtn_0d_usa that inherits a foreign key id from states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-       state_id INT NOT NULL,
+       state_id NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id) )
+       state_id INT FOREIGN KEY REFERENCES hbtn_0d_usa.states(id) )
        
