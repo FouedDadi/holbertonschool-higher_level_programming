94be058b201c68e6cb27e2bf2606f01906786d7d
FouedDadi <foued.dadii@gmail.com>
2020-08-09 17:20:09 +0100

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+safe from injection script that lists all states
+that matches the argument from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         password=sys.argv[2],
+                         database=sys.argv[3],
+                         port=3306)
+    cu = db.cursor()
+    cu.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN\
+                   states ON cities.state_id = states.id ORDER BY id")
+    result = cu.fetchall()
+    for i in range(len(result)):
+        print(result[i])
+    cu.close()
+    db.close()
