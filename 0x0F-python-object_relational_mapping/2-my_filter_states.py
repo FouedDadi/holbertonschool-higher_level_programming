3e52c5f4ba5756bfe33053d3080c0f279421f4a8
FouedDadi <foued.dadii@gmail.com>
2020-08-09 17:02:46 +0100

    tasks 1 2 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all states that matches the argument from the database hbtn_0e_0_usa
+lists all states that matches the argument from the database hbtn_0e_0_usa
 """
 
 
@@ -8,11 +8,16 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db= MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
-cursor= db.cursor()
-cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id".format(sys.argv[4]))
-result = cursor.fetchall()
-for i in range(len(result)):
-	print(result[i])
-cursor.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         password=sys.argv[2],
+                         database=sys.argv[3],
+                         port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id"
+                   .format(sys.argv[4]))
+    result = cursor.fetchall()
+    for i in range(len(result)):
+        print(result[i])
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-safe from injection script that lists all states that matches the argument from the database hbtn_0e_0_usa
+safe from injection script that lists all states
+that matches the argument from the database hbtn_0e_0_usa
 """
 
 
@@ -8,11 +9,16 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db= MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
-cursor= db.cursor()
-cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id",(sys.argv[4],))
-result = cursor.fetchall()
-for i in range(len(result)):
-	print(result[i])
-cursor.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         password=sys.argv[2],
+                         database=sys.argv[3],
+                         port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id",
+                   (sys.argv[4],))
+    result = cursor.fetchall()
+    for i in range(len(result)):
+        print(result[i])
+    cursor.close()
+    db.close()
