02c02b579ee885ddaa30e178eb7e6746a77f6874
FouedDadi <foued.dadii@gmail.com>
2020-08-18 17:43:03 +0100

    send json post request and display body's response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that sends a JSON POST request to a URL, and displays the body of the response.
-curl -LsX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -LsX POST -H "Content-Type: application/json" "$1" -d @"$2"
