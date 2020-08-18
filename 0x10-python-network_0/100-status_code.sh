4e5d361562fdc8c807a1fae498197dfea6fa9f9d
FouedDadi <foued.dadii@gmail.com>
2020-08-18 17:29:09 +0100

    send request and display status code of response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that sends a request and displays only the status code of the response.
-curl -o "$1"/dev/null -s -w "%{http_code}\n" "$1"
+curl -o "$1"/dev/null -s -w "%{http_code}" "$1"
