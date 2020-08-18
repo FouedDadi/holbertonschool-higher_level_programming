9317cd55bfb1be8c5ac3bdeac263450acc249892
FouedDadi <foued.dadii@gmail.com>
2020-08-18 15:37:13 +0100

    send request to url and get size of reponse

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 #Bash script that takes in a URL, sends a request to that URL,
 #and displays the size of the body of the response
-curl -sI $URL | grep -i Content-Length
+curl -sI "$URL" | grep -i Content-Length | cut -d " " -f2
