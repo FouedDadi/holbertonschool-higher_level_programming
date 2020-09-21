a8b3343a3b81c563d5a5f6450cfa8f47bd9eaef4
FouedDadi <foued.dadii@gmail.com>
2020-09-21 18:27:51 +0100

    print rectangle using c and x

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const rectangle = require('./5-square.js');
+class Square extends rectangle {
+  charPrint (c) {
+    if (c === undefined) { super.print(); } else {
+      let x = 0;
+      for (x = 0; x < this.height; x++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
