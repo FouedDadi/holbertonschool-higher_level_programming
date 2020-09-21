e1f2f1bdcfbca367b52c74c25c5d200e16657a65
FouedDadi <foued.dadii@gmail.com>
2020-09-21 17:32:32 +0100

    print rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-      print(){
-        let x = 0;
-        for (x = 0; x < this.height; x++) {
-        console.log('X'.repeat(this.width));}}
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    let x = 0;
+    for (x = 0; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
